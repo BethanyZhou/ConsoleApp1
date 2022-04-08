100df1b10a4b7461705dfbabb8e913bf0062e88e
Beisi Zhou <zazbs@qq.com>
2022-04-08 17:43:59 +0800

    wip

---
+++ ConsoleApp1/AzureTokenCredential.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AzureTokenCredential : TokenCredential
+    {
+        public string AccessToken { get; set; }
+
+        private readonly Func<string> GetTokenImpl;
+
+        public AzureTokenCredential(string accessToken, Func<string> getTokenImpl = null)
+        {
+            AccessToken = accessToken;
+            GetTokenImpl = getTokenImpl;
+        }
+
+        public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            return new AccessToken(GetTokenImpl == null ? AccessToken : GetTokenImpl(),
+                DateTimeOffset.UtcNow);
+        }
+
+        public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            return new ValueTask<AccessToken>(this.GetToken(requestContext, cancellationToken));
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Core;
+using Azure.ResourceManager;
+
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var accessToken = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "../../../", "accessToken.txt"));
+            TokenCredential token = new AzureTokenCredential(accessToken, null);
+            ArmClient armClient = new ArmClient(token);
+           
+            foreach (var provider in armClient.GetTenantProviders())
+            {
+                Console.WriteLine(provider.Namespace);
+            }
+        }
+    }
+}
